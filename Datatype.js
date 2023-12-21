f9f616e75805677658d45dd04178518938739e36
Krishna Paul <krishhmail01@gmail.com>
2023-12-21 14:55:17 +0530

    Js Basic

---
+++ Datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs not browser
+
+
+console.log(3+3); console.log("Hello")
+
+
+let name = "Krishna"
+
+let age = 21
+
+let isLoggedIn = true
+
+
+
+// number => 2 to 53
+// bigint
+
+// String => ""
+// boolean => true/false
+
+// null => standalone value
+
+// undefined => 
+
+// symbol
+
+
+// non primitive datatype
+
+//object
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null) // Object
---
+++ Test.js
@@ -0,0 +1,18 @@
+console.log('Hello');
+console.log("Hello");
+console.log(4+5);
+
+/*Never use var , because of issue in block and
+functional scope*/
+
+{
+    var b = 100
+{
+ let c = 20
+}
+    var a = 10
+    console.log(c)
+
+}
+
+
