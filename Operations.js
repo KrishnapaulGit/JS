2711cff84f055d64e0c8ed6ce7d2cf0bc4698dca
Krishna Paul <krishhmail01@gmail.com>
2023-12-21 21:42:43 +0530

    Basic Operatioms

---
+++ Operations.js
@@ -0,0 +1,55 @@
+// let value = 5
+
+// // let negValue=-value
+
+// // console.log(negValue);  // -5
+
+// console.log(3*3);  //9
+// console.log(3**3);  // 27 (3 to the power 3)
+// console.log(3+3);  // 6
+// console.log(2/3); // 0.6666
+// console.log(6%3); //2
+
+
+
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 + 3);
+// console.log(7+8+5);
+
+// Type Conversion Document link
+
+// https://tc39.es/ecma262/2023/#sec-type-conversion
+
+
+// Prefix , Postfix and Precidence
+
+// let count = 10
+
+// let isCount = count++
+// console.log(isCount);
+// isCount = count
+
+// console.log(isCount);
+
+// isCount = ++count
+// console.log(isCount);
+// console.log(count);
+
+let x = 3;
+const y = ++x;
+
+console.log(x);
+console.log(y);
+
+let x2 = 3n;
+const y2 = x2++;
+
+console.log(x2);
+console.log(y2);
+
+
+
+
+
+
+
