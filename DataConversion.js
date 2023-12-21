99fd1767359716e19bdbad129b646c4d27e0d56f
Krishna Paul <krishhmail01@gmail.com>
2023-12-21 19:22:03 +0530

    Data Type Conversion

---
+++ DataConversion.js
@@ -0,0 +1,57 @@
+// let age = "35"
+
+// console.log(typeof age)
+// let isNum = Number(age)
+// console.log(age)
+// console.log(typeof isNum)
+
+// let isString = String(age)
+// console.log(age)
+// console.log(typeof isString)
+
+
+let name = undefined
+
+console.log(typeof name);
+
+let isName = Number(name)
+
+console.log(isName);
+console.log(typeof(isName));
+
+/* 
+
+String -> Number => NaN(not a number and type is number)
+
+35Krishna => NaN
+35Krishna => true
+""  => 0
+
+null => object(type) => 0
+
+undefined => undefined(type) => NaN
+
+*/
+
+// let num = 35
+
+// let isString = Boolean(num)
+// console.log(isString);
+
+
+// let some = "1"
+
+// let something = Boolean(some)
+// console.log(typeof something);
+// console.log(something);
+
+/*
+ null => boolean => false
+ undefined => boolean => false
+ " " => boolean => false
+ "1" => boolean => true
+
+
+
+*/
+
