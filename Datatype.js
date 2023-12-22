596fd1d201a5e42e460479836d2dc937cc3de16d
Krishna Paul <krishhmail01@gmail.com>
2023-12-22 14:46:39 +0530

    Updated Datatype

--- Datatype.js
+++ Datatype.js
@@ -1,35 +1,136 @@
-"use strict"; // treat all JS code as newer version
+// // // "use strict"; // treat all JS code as newer version
 
-// alert("Hello") // we are using nodejs not browser
-
-
-console.log(3+3); console.log("Hello")
+// // // // alert("Hello") // we are using nodejs not browser
 
 
-let name = "Krishna"
-
-let age = 21
-
-let isLoggedIn = true
+// // // console.log(3+3); console.log("Hello")
 
 
+// // // let name = "Krishna"
 
-// number => 2 to 53
-// bigint
+// // // let age = 21
 
-// String => ""
-// boolean => true/false
-
-// null => standalone value
-
-// undefined => 
-
-// symbol
+// // // let isLoggedIn = true
 
 
-// non primitive datatype
 
-//object
+// // // // number => 2 to 53
+// // // // bigint
 
-console.log(typeof undefined)  // undefined
-console.log(typeof null) // Object
+// // // // String => ""
+// // // // boolean => true/false
+
+// // // // null => standalone value
+
+// // // // undefined => 
+
+// // // // symbol
+
+
+// // // // non primitive datatype
+
+// // // //object
+
+// // // console.log(typeof undefined)  // undefined
+// // // console.log(typeof null) // Object
+
+
+
+// // /*
+// // JavaScript is a dynamically typed language, 
+// // which means that data types of variables are determined by the value they 
+// // hold at runtime and can change throughout the program as we assign different values to them
+
+
+
+
+// // // Primitive Datatypes (Call by value)
+
+// // // 7 types 
+// // 1. String
+// // 2. Number
+// // 3. Boolean
+// // 4. null
+// // 5. undefined
+// // 6. Symbol
+// // 7. BigInt
+
+// // */
+
+// // /*
+// // // Reference (Non Primitive)
+
+// // 1. Array
+// // 2. Objects
+// // 3. Functions
+
+
+// // */
+
+
+// // // const age = "Nine"
+
+// // // console.log(age);
+// // // console.log(typeof age);
+// // // age = 100;
+// // // console.log(age);
+// // // console.log(typeof age);
+
+
+
+
+// // // const score = 100
+// // // console.log(score);
+
+// // // const scoreVal = 100.5
+// // // console.log(scoreVal);
+
+
+
+// // // let userEmail; // let userEmail=undefined;
+
+
+// // // Symbol
+
+// // // const id = Symbol('123');
+// // // const id2 = Symbol('123');
+
+// // // console.log(id==id2);//false
+
+
+// // // bigIng
+
+// // // const bigInt= 234567n;
+// // // console.log(bigInt);
+
+
+
+
+// // const heros = ["Shaktiman", "Krishh","Nagraaj"]
+
+// // console.log(heros);
+
+// // let details = {
+// //     name:"Krishna",
+// //     age:21
+
+// // }
+// // console.log(details);
+// // console.log(details.name+ "   " + details.age);
+
+
+
+// // Function
+
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
