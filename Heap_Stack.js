48cfaa9a99554226b74dac677da9f9b646e6f7ac
Krishna Paul <krishhmail01@gmail.com>
2023-12-22 16:45:10 +0530

    Basics of stack & Heap

---
+++ Heap_Stack.js
@@ -0,0 +1,41 @@
+// let name = "Krishna"
+// console.log(name);
+
+// let name2 = name
+
+// console.log(name2);
+
+// name2="Krishna Paul"
+
+// console.log(name2);
+// console.log(name);
+
+
+// //  All the Primitive data store in Stack , and we get only copy of the data
+// // Thats why it is called call by value
+
+
+let userDetails ={
+    name: "Krishna",
+    age : 21,
+    email: "krishhmail@gmail.com"
+}
+
+
+let userDetails1 = userDetails
+
+console.log(userDetails);
+console.log(userDetails1);
+
+userDetails1.email="krishnaofficial@gmail.com"
+console.log(userDetails.email);
+console.log(userDetails1.email);
+
+/**
+ Heap - All Non = Primitive datatypes store and we get the reference , thats why we call 
+ it Call by Reference
+ */
+
+
+
+
